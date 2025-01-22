7af5363c7ba1adaa8a7f69f5ecfde2c188b03942
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-01-22 21:48:10 +0700

    initial commit

--- app/src/main/java/com/example/repro/ui/auth/LogoutActivity.java
+++ app/src/main/java/com/example/repro/ui/auth/LogoutActivity.java
@@ -12,8 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.repro.ui.auth.LoginActivity;
-
 public class LogoutActivity extends Fragment {
 
     @Nullable
