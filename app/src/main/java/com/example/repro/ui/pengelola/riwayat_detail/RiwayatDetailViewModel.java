9f8c03d9f0fba3b32b77bc96348e6458d65c3739
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-01-23 22:21:22 +0700

    initial commit

---
+++ app/src/main/java/com/example/repro/ui/pengelola/riwayat_detail/RiwayatDetailViewModel.java
@@ -0,0 +1,7 @@
+package com.example.repro.ui.pengelola.riwayat_detail;
+
+import androidx.lifecycle.ViewModel;
+
+public class RiwayatDetailViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
